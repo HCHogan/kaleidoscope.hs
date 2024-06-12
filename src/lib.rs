485285f797347c084bb7fcd2104bfe9892ea1e6a
hank <ysh2291939848@outlook.com>
2024-06-12 18:05:03 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use hs_bindgen::*;
+
+#[hs_bindgen]
+pub fn add(left: i64, right: i64) -> i64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
